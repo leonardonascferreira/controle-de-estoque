5884fd6ae911725b0cdee45f7db119482d92bc93
Leonardo Ferreira <leonardonascferreira@gmail.com>
2026-01-07 20:03:48 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -1,7 +1,8 @@
-print('''MENU DE OPÇÕES
-[ 0 ] Adicionar Produto
-[ 1 ] Atualizar Produto
-[ 2 ] Excluir Produto
-[ 3 ] Visualizar Estoque
-[ 4 ] Sair do Sistema
-      ''')
+def exibir_menu():
+      print('''MENU DE OPÇÕES
+      [ 0 ] Adicionar Produto
+      [ 1 ] Atualizar Produto
+      [ 2 ] Excluir Produto
+      [ 3 ] Visualizar Estoque
+      [ 4 ] Sair do Sistema
+            ''')
