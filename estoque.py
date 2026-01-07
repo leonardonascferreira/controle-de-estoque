36a25f432c886969ab53451bceffe7a352f2a73d
Leonardo Ferreira <leonardonascferreira@gmail.com>
2026-01-07 20:11:45 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -4,10 +4,11 @@ def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def exibir_menu():
-      print('''MENU DE OPÇÕES
-      [ 0 ] Adicionar Produto
-      [ 1 ] Atualizar Produto
-      [ 2 ] Excluir Produto
-      [ 3 ] Visualizar Estoque
-      [ 4 ] Sair do Sistema
-            ''')
+      print('''==== MENU DE OPÇÕES ====
+[ 0 ] Adicionar Produto
+[ 1 ] Atualizar Produto
+[ 2 ] Excluir Produto
+[ 3 ] Visualizar Estoque
+[ 4 ] Sair do Sistema
+''')
+
