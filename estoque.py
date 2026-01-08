100d33ed0f0d1c530b404a5018605dbf1b0dc991
Leonardo Ferreira <leonardonascferreira@gmail.com>
2026-01-07 21:41:04 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -1,8 +1,17 @@
 import os
+import json
+from colorama import Fore, init
+init(autoreset=True)
 
+arquivo_estoque = 'estoque.json'
+
+
+
+#Função limpar a tela
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+#Função exibir menu
 def exibir_menu():
       print('''==== MENU DE OPÇÕES ====
 [ 0 ] Adicionar Produto
@@ -12,3 +21,17 @@ def exibir_menu():
 [ 4 ] Sair do Sistema
 ''')
 
+#Função adicionar produtos
+def adicionar_produto():
+    nome_produto = str(input('Digite o nome do produto: '))
+    preco_produto = float(input('Digite o preço do produto: R$'))
+    qtde_estoque = float(input('Quantidade no estoque: '))
+
+def atualizar_produto():
+     
+def excluir_produto():
+     
+def visualizar_estoque():
+     
+
+exibir_menu()
